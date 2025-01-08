9325b8fa78ebcbd323124bcc2caa241c2e404457
Monssaf Toukal <tmonssaf@gmail.com>
2025-01-08 15:49:10 -0500

    up neogit conf

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -5,20 +5,11 @@ return {
     "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
     "ibhagwan/fzf-lua", -- optional
-    "echasnovski/mini.pick", -- optional
   },
   config = function()
     local neogit = require("neogit")
     neogit.setup({
-      git_services = {
-        ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-        ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-        ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-        ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
-        ["stash.ovh.net"] = "https://stash.ovh.net/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-      },
       integrations = {
         diffview = true,
       },
